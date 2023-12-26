16a4e63330a606a01cccaed2ba43a9796db64481
SaekiTominaga <kcsu3470+github@gmail.com>
2023-12-26 22:52:53 +0900

    htmlelement-text 機能を移行

---
+++ javascript/htmlelement-text/src/HtmlElementText.ts
@@ -0,0 +1,37 @@
+/**
+ * HTMLElement text
+ */
+export default class {
+	readonly #thisElement: HTMLElement; // 対象要素
+
+	/**
+	 * @param thisElement - Target element
+	 */
+	constructor(thisElement: HTMLElement) {
+		this.#thisElement = thisElement;
+	}
+
+	/**
+	 * Get the text width (the width of that inline box)
+	 *
+	 * @param text - Text to calculate width (If not specified, HTMLElement.textContent)
+	 *
+	 * @returns Text width in CSS pixels
+	 */
+	getWidth(text = this.#thisElement.textContent): number {
+		if (text === null || text.length === 0) {
+			return 0;
+		}
+
+		const context = document.createElement('canvas').getContext('2d');
+		if (context === null) {
+			throw new Error('Failed to get a drawing context on the canvas.');
+		}
+		context.beginPath();
+
+		const styleDeclaration = getComputedStyle(this.#thisElement, '');
+		context.font = `${styleDeclaration.fontStyle} ${styleDeclaration.fontVariant} ${styleDeclaration.fontWeight} ${styleDeclaration.fontSize} ${styleDeclaration.fontFamily}`;
+
+		return context.measureText(text).width;
+	}
+}
